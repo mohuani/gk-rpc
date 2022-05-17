21cf3c8efe348f2e871cee4f23c413af936a2ae0
mohuani <wfk2975019671@sina.com>
2022-05-17 11:45:43 +0800

    feat: transport 模块

---
+++ gk-rpc-transport/src/main/java/com/smgeek/gkrpc/transport/RequestHandler.java
@@ -0,0 +1,11 @@
+package com.smgeek.gkrpc.transport;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 处理网络请求的Handler
+ */
+public interface RequestHandler {
+    void onRequest(InputStream receive, OutputStream os);
+}
---
+++ gk-rpc-transport/src/main/java/com/smgeek/gkrpc/transport/impl/HttpTransportClient.java
@@ -0,0 +1,49 @@
+package com.smgeek.gkrpc.transport.impl;
+
+import com.smgeek.gkrpc.Peer;
+import com.smgeek.gkrpc.transport.TransportClient;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpTransportClient implements TransportClient {
+    private String url;
+
+    @Override
+    public void connect(Peer peer) {
+        this.url = "http://" + peer.getHost() + ":" + peer.getPort();
+    }
+
+    @Override
+    public InputStream write(InputStream data) {
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) new URL(url).openConnection();
+
+            httpConn.setDoOutput(true);
+            httpConn.setDoInput(true);
+            httpConn.setUseCaches(false);
+            httpConn.setRequestMethod("POST");
+
+            httpConn.connect();
+            IOUtils.copy(data, httpConn.getOutputStream());
+
+            int responseCode = httpConn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                return httpConn.getInputStream();
+            } else {
+                return httpConn.getErrorStream();
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
